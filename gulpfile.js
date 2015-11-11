14e537d214b9ab286021f1b0d08c7e95eeae4331
Alex Swan <Alexander_Swan@fundslibrary.co.uk>
2015-11-11 12:19:42 +0000

    Really simple static site generator based on gulp-replace-task only

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+var gulp = require('gulp');
+var del = require("del");
+var replace = require("gulp-replace-task");
+var glob = require("glob");
+var merge = require('merge-stream');
+var fs = require('fs-extra');
+var rename = require("gulp-rename");
+
+function readFileIfExists(file) {
+    try
+    {
+        var contents = fs.readFileSync(file, { encoding: "utf8" });
+        return contents;
+    }
+    catch(e)
+    {
+        return '';
+    }
+}
+
+function getOnlyName(filePathFull){
+	var thing = filePathFull.split("/").reverse()[0];
+	console.log(thing);
+	return thing;
+}
+
+gulp.task("clean",function(){
+	return del(["bin"]);
+});
+
+gulp.task("build-views", function(){
+	var views = glob.sync("./views/pages/**/*.html");
+	
+	var pipes = views.map(function(view){
+		console.log(view);
+		var newFilePath = view.replace("./views/pages/", "");
+		console.log(newFilePath);
+		return gulp.src("views/template.html")
+			.pipe(rename(newFilePath))
+			.pipe(replace({
+				patterns: [
+					{
+						match: 'body',
+						replacement: readFileIfExists(view)
+					}]
+			}))
+			.pipe(gulp.dest("./bin/views"));
+	});
+	
+	return merge(pipes);
+});
+
+gulp.task("default", ["clean", "build-views"]);
